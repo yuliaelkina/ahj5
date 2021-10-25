462338df10b0e707d0698658c3290200898ce1c1
yulia elkina <todg@mail.ru>
2021-10-25 19:42:33 +0300

    tests changed

---
+++ src/e2e/e2e.test.js
@@ -0,0 +1,43 @@
+import puppetteer from "puppeteer";
+import { fork } from "child_process";
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe("Credit Card Validator form", () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = "http://localhost:9000";
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on("error", reject);
+      server.on("message", (message) => {
+        if (message === "ok") {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppetteer.launch({
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test("Popover Renderer", async () => {
+    await page.goto(baseUrl);
+    const button = await page.$("button");
+    await button.click();
+    await page.waitForSelector("[class=popover]");
+    await browser.close();
+  });
+});
